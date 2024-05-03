f7310f671b51b64ec8f755ad7d79aa179d16c0eb
gian2830 <167557622+gian2830@users.noreply.github.com>
2024-05-03 10:48:00 -0500

    codigo lab1

---
+++ Solution1/laboratorio 1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//sumar los n numeros primeros
+var n = 2;
+var suma = 0;
+for (var i = 1; i <= n; i++)
+
+{
+    suma += i;
+}
+
+Console.WriteLine("la suma de los {n} numeros naturales son : {suma}");
