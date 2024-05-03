62e2f824909d7e9ad5fa10d8fcd0731697cda79a
gian2830 <167557622+gian2830@users.noreply.github.com>
2024-05-03 11:48:30 -0500

    codigo lab1

--- Solution1/laboratorio 1/Program.cs
+++ Solution1/laboratorio 1/Program.cs
@@ -1,11 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//sumar los n numeros primeros
-var n = 2;
-var suma = 0;
-for (var i = 1; i <= n; i++)
+using System;
 
+class Calculadora
 {
-    suma += i;
-}
+    static void Main()
+    {
+        // Solicitar los dos números al usuario
+        Console.Write("Ingresa el primer número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("la suma de los {n} numeros naturales son : {suma}");
+        Console.Write("Ingresa el segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        // Realizar las operaciones aritméticas
+        double suma = num1 + num2;
+        double resta = num1 - num2;
+        double multiplicacion = num1 * num2;
+        double division;
+
+        // Imprimir los resultados
+        Console.WriteLine("\nResultados:");
+        Console.WriteLine($"Suma: {num1} + {num2} = {suma}");
+        Console.WriteLine($"Resta: {num1} - {num2} = {resta}");
+        Console.WriteLine($"Multiplicación: {num1} * {num2} = {multiplicacion}");
+
+        // Manejar la división por cero
+        if (num2 != 0)
+        {
+            division = num1 / num2;
+            Console.WriteLine($"División: {num1} / {num2} = {division}");
+        }
+        else
+        {
+            Console.WriteLine($"División: No se puede dividir por cero.");
+        }
+    }
+}
